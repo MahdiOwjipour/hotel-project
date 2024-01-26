5dc6ec35cbfd376acc59f1ca6bb753f5155ddf3c
Mahdi <seyedmpowr@gmail.com>
2024-01-27 01:24:54 +0330

    some functions added

--- admin.py
+++ admin.py
@@ -1,19 +1,36 @@
 from person import *
 from room import *
 from staff import *
+from hotel import *
 
 class Admin(Person):
     def __init__(self, id_, first_name, last_name, age):
         super().__init__(id_, first_name, last_name, age)
+        self.staff_dict = {} # staff members
 
-    def create_staff_account(self, staff_details):
-        pass
+    def create_staff_account(self, new_staff):
+        self.id_ = input("new staff id: ")
+        self.first_name = input("new staff first name: ")
+        self.last_name = input("new staff last name: ")
+        self.age = input("new staff age: ")
+        self.role = input("new staff role: ")
+        if new_staff.id_ not in self.staff_dict:
+            self.staff_dict[new_staff.id_] = new_staff
+        else:
+            print("id already existed!")
 
     def remove_staff_member(self, staff_id):
-        pass
+        for i in self.staff_dict:
+            if i == staff_id:
+                self.staff_dict.pop(i)
+            else:
+                print("id does not exist!")
 
     def update_staff_role(self, staff_id, new_role):
-        pass
+        if staff_id in self.staff_dict:
+            staff_id[self.role] = new_role or self.role
+        else:
+            print("id does not exist!")
 
     def approve_maintenance_request(self, room_id, maintenance_type):
         pass
